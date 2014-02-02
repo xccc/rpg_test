5744a063bca9c26386d4b5a5a70a9df4852e3426
root <root@localhost.localdomain>
2014-02-02 14:33:59 +0200

    lol

--- public/menu.js
+++ public/menu.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#listb").click(function(){
+  $("#ip_list").click(function(){
     $("#iplist").toggle();
   });
 });
@@ -8,4 +8,52 @@ $(document).ready(function(){
   $("#listb2").click(function(){
     $("#iplist").toggle();
   });
+});
+
+$(function() {
+  $( "#host" ).draggable();
+  $( "#droppable1" ).droppable({
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "p" )
+         .html( "Dropped 1 !" );
+      }
+   });
+});
+
+$(function() {
+  $( "listb2" ).draggable();
+  $( "#droppable2" ).droppable({
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "p" )
+         .html( "Dropped 2!" );
+      }
+   });
+});
+
+$(function() {
+  $( "#listb2" ).draggable();
+  $( "#droppable3" ).droppable({
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "p" )
+         .html( "Dropped 3!" );
+      }
+   });
+});
+
+$(function() {
+  $( "#listb2" ).draggable();
+  $( "#droppable4" ).droppable({
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "p" )
+         .html( "Dropped into 4!" );
+      }
+   });
 });
